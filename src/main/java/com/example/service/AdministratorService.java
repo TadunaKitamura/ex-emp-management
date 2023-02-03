d9b8f8ff8934dfb337e30749eab09f414ea93052
TadunaKitamura <taduna.kitamura0324@gmail.com>
2023-02-03 14:46:44 +0900

    AdministratorService,AdministratorFormの実装

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,5 +1,22 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.repository.AdministratorRepository;
+
+/**
+ * Administratorのサービスクラス
+ * 
+ * @author tadunakitamura
+ *
+ */
+@Service
+@Transactional
 public class AdministratorService {
+
+	@Autowired
+	private AdministratorRepository administratorRepository;
 
 }

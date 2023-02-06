8f2247f3ca29d6bba40840a85a90844439d983fe
TadunaKitamura <taduna.kitamura0324@gmail.com>
2023-02-06 13:44:27 +0900

    EmployeeServiceクラスの実装

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+/**
+ * 従業員のサービスクラス.
+ * 
+ * @author tadunakitamura
+ *
+ */
+@Service
+@Transactional
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	/**
+	 * 従業員情報を全件取得する.
+	 * 
+	 * @return 従業員情報のリスト
+	 */
+	public List<Employee> showList() {
+
+		List<Employee> employeeList = employeeRepository.findAll();
+		return employeeList;
+
+	}
+
+}
